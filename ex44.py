7e22fb023715bd24a6c2defe136982a868ef3309
yoyo2609 <90603828+yoyo2609@users.noreply.github.com>
2021-10-14 17:10:09 +0200

    Add files via upload

--- ex44.py
+++ ex44.py
@@ -1,12 +1,5 @@
-a = int(input("longueur a:"))
-b = int(input("longueur b:"))
-c = int(input("longueur c:"))
-if c<a+b:
-    if a==b==c:
-        print("le triangle est equilateral")
-    elif a==b or b==c or a==c:
-        print("le triangle est isocele")
-    else:
-        print("le triangle est scalene")
-else:
-    print("triangle impossible")
+ab = int(input("longueur ab:"))
+bc = int(input("longueur bc:"))
+ac = int(input("longueur ac:"))
+if ab**2+bc**2==ac*ac:
+        print("le triangle est rectangle")
