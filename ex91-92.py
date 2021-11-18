16162420cd1d244c812485d942e5b962c509badb
yoyo2609 <90603828+yoyo2609@users.noreply.github.com>
2021-11-18 17:24:13 +0100

    Add files via upload

---
+++ ex91-92.py
@@ -0,0 +1,18 @@
+#ex90
+"""n=int(input("entrer un nombre strictement positif:"))
+e=0
+plusgrand = n
+while n!=1:
+     if n%2==0:
+         n=n//2
+     else :
+         n=n*3+1
+     if n>plusgrand:
+        plusgrand=n
+    e=e+1
+    print(n)
+print("nombre d'etapes" ,e)
+prin("nobmre maximal:",plusgrand)"""
+               
+
+
